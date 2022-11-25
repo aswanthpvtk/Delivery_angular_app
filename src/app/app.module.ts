8c6c7d841e52644fa761065a999e82af866c228e
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-25 07:25:34 +0530

    delivery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserRegComponent } from './user-reg/user-reg.component';
+import { VegComponent } from './veg/veg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserRegComponent
+    UserRegComponent,
+    VegComponent
   ],
   imports: [
     BrowserModule,
